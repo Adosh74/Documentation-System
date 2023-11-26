5948931b9e4d4cd930670e86953dcb6f2a84f18e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-26 04:54:14 +0200

    create graphql endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import envConfig from './config/env.config';
+import { resolvers, typeDefs } from './graphql/index.graphql';
 
 export const app = express();
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
 
 //middleware
 app.use(cors());
@@ -13,7 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 if (envConfig.env === 'development') app.use(morgan('dev'));
 
 // root route
-app.get('/', (req, res) => {
+app.use('/graphql', expressMiddleware(server));
+
+app.get('/', (req: Request, res: Response) => {
 	res.send('Documentation system for the API(GraphQL)');
 });
 
