23dec20135d9836b49ee195276bbf11a3ad40784
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-27 00:41:45 +0200

    update index.graphql.ts

---
+++ src/graphql/index.graphql.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import { projectResolver } from './resolvers/project.resolver';
+
+// read the typeDefs from the typeDefs folder
+const projectType = fs.readFileSync(
+	path.join(__dirname, './typeDefs/project.graphql'),
+	'utf-8'
+);
+const srsType = fs.readFileSync(path.join(__dirname, './typeDefs/srs.graphql'), 'utf-8');
+const sddType = fs.readFileSync(path.join(__dirname, './typeDefs/sdd.graphql'), 'utf-8');
+
+export const typeDefs = `
+    ${projectType}
+	${srsType}
+	${sddType}
+`;
+
+export const resolvers = {
+	Query: {
+		...projectResolver.Query,
+	},
+	Mutation: {
+		...projectResolver.Mutation,
+	},
+};
