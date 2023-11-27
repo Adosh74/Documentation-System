d0d696776f3b4489a3814c06f5458ae507d073eb
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-27 04:04:16 +0200

    create "get all and get one sdd"

---
+++ src/graphql/services/sdd.service.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+import { GraphQLResolveInfo } from 'graphql';
+import { extractSelections } from '../utils/extractSelections';
+
+interface GetSddsArgs {
+	info: GraphQLResolveInfo;
+}
+
+interface GetSddArgs extends GetSddsArgs {
+	id: string;
+}
+
+interface SddInput {
+	uml: [string];
+	projectId: string;
+}
+
+const prisma = new PrismaClient();
+
+/// ***  1. get all sdds *** ///
+export const getSdds = async ({ info }: GetSddsArgs) => {
+	const extractedSelections = extractSelections(info);
+	const projectIncluded = extractedSelections.includes('project');
+
+	if (projectIncluded) return await prisma.sDD.findMany({ include: { project: true } });
+
+	return await prisma.sDD.findMany();
+};
+
+/// ***  2. get a single sdd *** ///
+export const getSdd = async ({ id, info }: GetSddArgs) => {
+	const extractedSelections = extractSelections(info);
+	const projectIncluded = extractedSelections.includes('project');
+
+	if (projectIncluded)
+		return await prisma.sDD.findUnique({
+			where: { id },
+			include: { project: true },
+		});
+
+	return await prisma.sDD.findUnique({ where: { id } });
+};
