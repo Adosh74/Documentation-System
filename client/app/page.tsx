b19b1e8f661309fdf118120128e20d2bf3b94272
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-29 22:58:20 +0200

    formating

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,15 @@
-"use client";
+'use client';
 
-import Header from "@/components/Header";
-import styles from"./page.module.css"
+import Header from '@/components/Header';
+import styles from './page.module.css';
 
 function Home() {
-  
-  return (
-    
-    <main>
-      <Header />
-      < div className={styles.home}> 
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+			<div className={styles.home}></div>
+		</main>
+	);
 }
 
 export default Home;
