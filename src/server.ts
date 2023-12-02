4696423312858542e054beec42297b9f18f75b64
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-02 08:15:57 +0200

    update server file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from './app';
+import envConfig from './config/env.config';
 
 app.listen(3001, () => {
-	console.log('Server started at http://localhost:3001');
-	console.log('GraphQL server started at http://localhost:3001/graphql');
+	if (envConfig.env === 'development') {
+		console.log('Server started at http://localhost:3001');
+		console.log('GraphQL server started at http://localhost:3001/graphql');
+	}
 });
