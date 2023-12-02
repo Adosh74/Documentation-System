3412654664018f04f0d94e8b6ea9cfe0be69e5a4
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-02 08:15:42 +0200

    add catching error

--- src/app.ts
+++ src/app.ts
@@ -13,23 +13,37 @@ const server = new ApolloServer({
 	resolvers,
 });
 
-server.start().then(() => {
-	//middleware
-	app.use(cors());
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+server
+	.start()
+	.then(() => {
+		// *** middleware *** //
+		// allow cross origin requests from the frontend server
+		app.use(
+			cors({
+				origin: 'http://localhost:3000',
+				credentials: true,
+			})
+		);
+		// parse incoming requests
+		app.use(express.json({ limit: '50mb' }));
+		app.use(express.urlencoded({ extended: true }));
 
-	if (envConfig.env === 'development') app.use(morgan('dev'));
+		// logging
+		if (envConfig.env === 'development') app.use(morgan('dev'));
 
-	// root route
-	app.use('/graphql', expressMiddleware(server));
+		// *** root route *** //
+		app.use('/graphql', expressMiddleware(server));
 
-	app.get('/', (req: Request, res: Response) => {
-		res.send('Documentation system for the API(GraphQL)');
+		app.get('/', (req: Request, res: Response) => {
+			res.send('Documentation system for the API(GraphQL)');
+		});
+
+		// monitor the health of the application
+		app.get('/healthz', (req: Request, res: Response) =>
+			res.status(200).json({ success: true, message: 'Ok' })
+		);
+	})
+	.catch((err) => {
+		console.log(err);
+		process.exit(1);
 	});
-
-	// monitor the health of the application
-	app.get('/healthz', (req: Request, res: Response) =>
-		res.status(200).json({ success: true, message: 'Ok' })
-	);
-});
