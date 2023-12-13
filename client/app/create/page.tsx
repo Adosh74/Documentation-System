207ffa7ff573dee0b379aec5c88b2d822ca62a07
Mohamed Shaban <mohamed.shabanzakaryia@gmail.com>
2023-12-13 14:13:23 +0200

    update client

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -2,14 +2,30 @@
 
 import ChoosePhase from '@/components/ChoosePhase';
 import Header from '@/components/Header';
+import Initiation from '@/components/Initiation';
 import { useReducer } from 'react';
 import styles from './page.module.css';
+
+const initialProjectInfo = {
+	title: '',
+	startDate: null,
+	finishDate: null,
+	objectives: '',
+	projectManager: '',
+	budget: '',
+	scopeStatements: '',
+};
 
 function create() {
 	return (
 		<>
 			<Header />
-			<ChoosePhase />
+			<Initiation
+				onSave={() => {
+					return;
+				}}
+				initialProjectInfoo={initialProjectInfo}
+			/>
 
 			<div>
 				<div className={styles.section}></div>
