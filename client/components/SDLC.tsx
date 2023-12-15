7f79129a0b5ad95e0c50a23aa2ee36fbc0781ad5
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-15 09:54:20 +0200

    show specific project phases in one page

--- client/components/SDLC.tsx
+++ client/components/SDLC.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { Iinitiation, Isdd, Isrs } from '@/app/sdlc/page';
 import ViewInitiation from '@/components/ViewInitiation';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
 import React, { useState } from 'react';
 import styles from './Styles.module.css';
 import UpdateInitiation from './UpdateInitiation';
@@ -9,35 +13,45 @@ import UpdateSRS from './UpdateSRS';
 import ViewSDD from './ViewSDD';
 import ViewSRS from './ViewSRS';
 
-interface initiation {
-	id: string;
-	title?: string;
-	startIn?: Date;
-	endIn?: Date;
-	objectives?: string;
-	project_manager?: string;
-	budget?: number;
-	scope?: string;
-}
-interface srs {
-	introduction: string;
-	purposeOfSoftwareBeingDeveloped: string;
-	intendedAudience: string;
-	overallDescriptionOfTheSoftware: string;
-	systemFeaturesAndRequirements: string;
-	browserImage: string;
-}
+export const client = new ApolloClient({
+	link: createUploadLink({
+		uri: 'http://localhost:3001/graphql',
+		headers: { 'Apollo-Require-Preflight': 'true' },
+	}),
+	cache: new InMemoryCache(),
+});
 
-interface sdd {
-	id: number;
-	file: string;
-	fileName: string;
-}
+// graphql mutation for delete sdd
+const DeleteSdd = gql`
+	mutation DeleteSdd($deleteSddId: String!) {
+		deleteSdd(id: $deleteSddId) {
+			uml
+		}
+	}
+`;
+
+// delete srs mutation
+const DeleteSrs = gql`
+	mutation DeleteSrs($deleteSrsId: String!) {
+		deleteSrs(id: $deleteSrsId) {
+			intro
+		}
+	}
+`;
+
+// delete initiation mutation
+const DeleteProject = gql`
+	mutation DeleteProject($deleteProjectId: String!) {
+		deleteProject(id: $deleteProjectId) {
+			title
+		}
+	}
+`;
 
 interface Info {
-	InitiationProjectInfo: initiation;
-	SRSProjectInfo: srs;
-	SDDProjectInfo: sdd[];
+	InitiationProjectInfo: Iinitiation;
+	SRSProjectInfo: Isrs;
+	SDDProjectInfo: Isdd;
 }
 
 const SDLC: React.FC<Info> = ({
@@ -45,13 +59,20 @@ const SDLC: React.FC<Info> = ({
 	SRSProjectInfo,
 	SDDProjectInfo,
 }) => {
-	const [projectInfo1, setProjectInfo1] = useState<any>(InitiationProjectInfo);
-	const [projectInfo2, setProjectInfo2] = useState<any>(SRSProjectInfo);
-	const [projectInfo3, setProjectInfo3] = useState<any>(SDDProjectInfo);
+	const [projectInfo1, setProjectInfo1] = useState<Iinitiation | any>(
+		InitiationProjectInfo
+	);
+	const [projectInfo2, setProjectInfo2] = useState<Isrs | any>(SRSProjectInfo);
+	const [projectInfo3, setProjectInfo3] = useState<Isdd | any>(SDDProjectInfo);
 
 	const [isEditing1, setIsEditing1] = useState<boolean>(false);
 	const [isEditing2, setIsEditing2] = useState<boolean>(false);
 	const [isEditing3, setIsEditing3] = useState<boolean>(false);
+
+	// use mutation
+	const [deleteSdd] = useMutation(DeleteSdd, { client });
+	const [deleteSrs] = useMutation(DeleteSrs, { client });
+	const [deleteProject] = useMutation(DeleteProject, { client });
 
 	const handleSave1 = (updatedInfo: any) => {
 		setProjectInfo1(updatedInfo);
@@ -60,7 +81,10 @@ const SDLC: React.FC<Info> = ({
 	const handleEdit1 = () => {
 		setIsEditing1(true);
 	};
-	const handleDelete1 = () => {
+	const handleDelete1 = async () => {
+		await deleteProject({ variables: { deleteProjectId: projectInfo1.id } });
+		// navigate to home page
+		window.location.href = '/';
 		setProjectInfo1(null);
 	};
 
@@ -71,7 +95,8 @@ const SDLC: React.FC<Info> = ({
 	const handleEdit2 = () => {
 		setIsEditing2(true);
 	};
-	const handleDelete2 = () => {
+	const handleDelete2 = async () => {
+		await deleteSrs({ variables: { deleteSrsId: projectInfo2.id } });
 		setProjectInfo2(null);
 	};
 
@@ -82,63 +107,58 @@ const SDLC: React.FC<Info> = ({
 	const handleEdit3 = () => {
 		setIsEditing3(true);
 	};
-	const handleDelete3 = () => {
+	const handleDelete3 = async () => {
+		await deleteSdd({ variables: { deleteSddId: projectInfo3.id } });
 		setProjectInfo3(null);
 	};
 
 	return (
-		<div className={styles.view}>
-			<div>
-				{isEditing1 ? (
-					<UpdateInitiation
-						onSave={handleSave1}
-						initialProjectInfoo={projectInfo1}
-					/>
-				) : (
-					projectInfo1 && (
-						<ViewInitiation
-							projectInfo={projectInfo1}
-							onEdit={handleEdit1}
-							onDelete={handleDelete1}
+		<ApolloProvider client={client}>
+			<div className={styles.view}>
+				<div>
+					{isEditing1 ? (
+						<UpdateInitiation
+							onSave={handleSave1}
+							initialProjectInfoo={projectInfo1}
 						/>
-					)
-				)}
+					) : (
+						projectInfo1 && (
+							<ViewInitiation
+								projectInfo={projectInfo1}
+								onEdit={handleEdit1}
+								onDelete={handleDelete1}
+							/>
+						)
+					)}
+				</div>
+				<div>
+					{isEditing2 ? (
+						<UpdateSRS onSave={handleSave2} srsPhaseInfo={projectInfo2} />
+					) : (
+						projectInfo2 && (
+							<ViewSRS
+								projectInfo={projectInfo2}
+								onEdit={handleEdit2}
+								onDelete={handleDelete2}
+							/>
+						)
+					)}
+				</div>
+				<div style={{ marginBottom: '0em' }}>
+					{isEditing3 ? (
+						<UpdateSDD onSave={handleSave3} sddPhaseInfo={projectInfo3} />
+					) : (
+						projectInfo3 && (
+							<ViewSDD
+								projectInfo={projectInfo3}
+								onEdit={handleEdit3}
+								onDelete={handleDelete3}
+							/>
+						)
+					)}
+				</div>
 			</div>
-			<div>
-				{isEditing2 ? (
-					<UpdateSRS
-						onSave={handleSave2}
-						initialProjectInfoo={projectInfo2}
-						projectId={''}
-					/>
-				) : (
-					projectInfo2 && (
-						<ViewSRS
-							projectInfo={projectInfo2}
-							onEdit={handleEdit2}
-							onDelete={handleDelete2}
-						/>
-					)
-				)}
-			</div>
-			<div style={{ marginBottom: '0em' }}>
-				{isEditing3 ? (
-					<UpdateSDD
-						onSave={handleSave3}
-						initialProjectInfoo={projectInfo3}
-						projectId={''}
-					/>
-				) : (
-					projectInfo3 && (
-						<ViewSDD
-							projectInfo={projectInfo3}
-							onEdit={handleEdit3}
-							onDelete={handleDelete3}
-						/>
-					)
-				)}
-			</div>
-		</div>
+		</ApolloProvider>
 	);
 };
 
